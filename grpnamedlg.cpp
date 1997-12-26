8e4736cc6ee9a9fea1e0c943ec3bac1dbb70380c
Denis Perchine II <dyp@perchine.com>
1997-12-26 22:02:04 +0000

    Forget something
    svn path=/trunk/kdeadmin/kuser/; revision=3649

---
+++ grpnamedlg.cpp
@@ -0,0 +1,111 @@
+#include "globals.h"
+#include <kmsgbox.h>
+#include <kbuttonbox.h>
+#include <qlayout.h>
+#include <qvalidator.h>
+#include "grpnamedlg.h"
+#include "grpnamedlg.moc"
+#include "misc.h"
+#include "maindlg.h"
+
+grpnamedlg::grpnamedlg(KGroup *agroup, QWidget* parent, const char* name)
+           :QDialog(parent, name, TRUE)
+{
+  group = agroup;
+  group->setgid(groups->first_free());
+
+  setCaption(_("Enter groupname"));
+
+  QVBoxLayout *layout = new QVBoxLayout(this, 10);
+  QGridLayout *grid = new QGridLayout(2, 2);
+  layout->addLayout(grid);
+
+  QLabel* lb1 = new QLabel(this, "lb1");
+  lb1->setText(_("Username:"));
+  lb1->setMinimumSize(lb1->sizeHint());
+  lb1->setAlignment(AlignRight|AlignVCenter);
+  grid->addWidget(lb1, 0, 0, AlignRight);
+
+  legrpname = new QLineEdit( this, "LineEdit_1" );
+
+  // ensure it fits at least 20 characters
+  legrpname->setText( "XXXXXXXXXXXXXXXXXXX" );
+  legrpname->setMinimumSize( legrpname->sizeHint() );
+
+  // clear text
+  legrpname->setText( "" );
+  legrpname->setFocus();
+  grid->addWidget(legrpname, 0, 1);
+
+  legid = new QLineEdit( this, "LineEdit_2" );
+
+  // ensure it fits at least 20 characters
+  legid->setText( "XXXXXXXXXXXXXXXXXXX" );
+  legid->setMinimumSize( legid->sizeHint() );
+  
+  QString s;
+  s.setNum(group->getgid());
+  
+  // clear text
+  legid->setText(s);
+  legid->setValidator(new QIntValidator(this, "val1"));
+
+  grid->addWidget(legid, 1, 1);
+  
+  // add a button box
+  KButtonBox *bbox = new KButtonBox(this);
+
+  // make buttons right aligned
+  bbox->addStretch(1);
+
+  // the default buttons  
+  pbOk = bbox->addButton(_("Ok"));
+  pbCancel = bbox->addButton(_("Cancel"));
+  pbOk->setDefault(TRUE);
+  
+  // establish callbacks
+  QObject::connect(pbOk, SIGNAL(clicked()), 
+		   this, SLOT(ok()));
+  QObject::connect(pbCancel, SIGNAL(clicked()), 
+		   this, SLOT(cancel()));
+
+  bbox->layout();
+  bbox->setMinimumSize(bbox->sizeHint());
+  
+  layout->addWidget(bbox);
+  layout->freeze();
+}
+
+grpnamedlg::~grpnamedlg() {
+  delete legrpname;
+  delete pbOk;
+  delete pbCancel;
+}
+
+void grpnamedlg::ok()
+{
+  QString tmp;
+  QString s;
+  s.setStr(legid->text());
+
+  if (groups->group_lookup(legrpname->text()) != NULL) {
+    tmp.sprintf(_("Group with name %s already exists."), legrpname->text());
+    KMsgBox::message(0, _("Error"), tmp, KMsgBox::STOP);
+    return;
+  }
+  
+  if (groups->group_lookup(s.toInt()) != NULL) {
+    tmp.sprintf(_("Group with gid %d already exists."), s.toInt());
+    KMsgBox::message(0, _("Error"), tmp, KMsgBox::STOP);
+    return;
+  }
+  
+  group->setname(legrpname->text());
+  group->setgid(s.toInt());
+  accept();
+}
+
+void grpnamedlg::cancel()
+{
+  reject();
+}

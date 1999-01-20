1ca841c362aa004f6ea9d750a4eb17313e90b44d
Denis Perchine II <dyp@perchine.com>
1999-01-20 16:12:43 +0000

    kuser-2056 bug fixed. Group file now has 644 perm. And /etc/yppasswd also has 644 perm when NIS enabled.
    svn path=/trunk/kdeadmin/kuser/; revision=15700

--- globals.h
+++ globals.h
@@ -22,9 +22,9 @@ extern int is_shadow;
 #ifdef _KU_NIS
   #define SHELL_FILE "/etc/shells"
   #define PASSWORD_FILE "/etc/yppasswd"
-  #define PASSWORD_FILE_MASK S_IRUSR | S_IWUSR
+  #define PASSWORD_FILE_MASK S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
   #define GROUP_FILE "/etc/ypgroup"
-  #define GROUP_FILE_MASK S_IRUSR | S_IWUSR
+  #define GROUP_FILE_MASK S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
   #undef _KU_SHADOW
   #define PWMKDB "cd /var/yp; make 2>&1 >> /var/log/kuser"
   #define GRMKDB "cd /var/yp; make 2>&1 >> /var/log/kuser"
@@ -40,7 +40,7 @@ extern int is_shadow;
     #define PASSWORD_FILE_MASK S_IRUSR | S_IWUSR
     #define PWMKDB _PATH_PWD_MKDB" -p "PASSWORD_FILE
     #define GROUP_FILE "/etc/group"
-    #define GROUP_FILE_MASK S_IRUSR | S_IWUSR
+    #define GROUP_FILE_MASK S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
     #define SKELDIR "/usr/share/skel"
     #define SKEL_FILE_PREFIX "dot"
   #else
@@ -48,7 +48,7 @@ extern int is_shadow;
     #define PASSWORD_FILE "/etc/passwd"
     #define PASSWORD_FILE_MASK S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
     #define GROUP_FILE "/etc/group"
-    #define GROUP_FILE_MASK S_IRUSR | S_IWUSR
+    #define GROUP_FILE_MASK S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
     #define SKELDIR "/etc/skel"
     #define SKEL_FILE_PREFIX ""
   #endif

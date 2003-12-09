7db91bd73038a79fd784734eb8990a567e710cde
Adriaan de Groot <groot@kde.org>
2003-12-09 21:51:21 +0000

    Adding copyright headers, as discussed on -devel and with Denis; using text  *  Copyright (c) 1998 Denis Perchine <dyp@perchine.com>  *  Maintained by Adriaan de Groot <groot@kde.org> plus standard GPL clause (copied from delUser.h)
    svn path=/trunk/kdeadmin/kuser/; revision=272216

--- userDefaultsPage.cpp
+++ userDefaultsPage.cpp
@@ -1,11 +1,22 @@
-/**********************************************************************
+/*
+ *  Copyright (c) 1998 Denis Perchine <dyp@perchine.com>
+ *  Maintained by Adriaan de Groot <groot@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License version 2 as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ **/
 
-	--- Qt Architect generated file ---
-
-	File: userDefaultsPage.cpp
-	Last generated: Mon Oct 12 00:56:45 1998
-
- *********************************************************************/
 
 
 #include <kapplication.h>

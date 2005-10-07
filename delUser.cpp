b25cbc1c7c05fb986d0fec6fb2d239e1e05dfb4c
Laurent Montel <montel@kde.org>
2005-10-07 13:57:05 +0000

    Launch my script
    svn path=/trunk/KDE/kdeadmin/kuser/; revision=468241

--- delUser.cpp
+++ delUser.cpp
@@ -28,7 +28,7 @@ delUser::delUser(KUser *AUser, QWidget *parent, const char *name)
  : KDialogBase( parent, name, true, i18n("Delete User"),
                 KDialogBase::Ok|KDialogBase::Cancel, KDialogBase::Ok, true )
 {                   
-  Q3VBox *page = makeVBoxMainWidget();
+  KVBox *page = makeVBoxMainWidget();
   new QLabel( i18n("<p>Deleting user <b>%1</b>"
                    "<p>Also perform the following actions:").arg(AUser->getName()),
               page);

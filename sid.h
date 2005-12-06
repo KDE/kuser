734fbdaad9f7e3ddd6c5501c51aab3df2a85cd0b
Thiago Macieira <thiago@kde.org>
2005-12-06 03:57:59 +0000

    Correcting the encoding in KDE source code
    svn path=/trunk/KDE/kdeadmin/kuser/; revision=485900

--- sid.h
+++ sid.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2004 Szombathelyi György <gyurco@freemail.hu>
+ *  Copyright (c) 2004 Szombathelyi GyÃ¶rgy <gyurco@freemail.hu>
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public

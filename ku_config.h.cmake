abcc93522c84eabfb7977bbcdf6845d27c6d8220
Laurent Montel <montel@kde.org>
2006-05-31 08:17:36 +0000

    Cleanup
    svn path=/trunk/KDE/kdeadmin/kuser/; revision=546854

--- ku_config.h.cmake
+++ ku_config.h.cmake
@@ -12,3 +12,12 @@
 #cmakedefine HAVE_FGETSPENT
 #cmakedefine HAVE_FGETPWENT
 #cmakedefine HAVE_FGETGRENT
+
+/* Define to 1 if you have the <paths.h> header file. */
+#cmakedefine HAVE_PATHS_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H 1
